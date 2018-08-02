463eba16fb4e3713a692b4b3317db83cefb6aaa3
Nam Tran <32343770+namtran98@users.noreply.github.com>
2018-08-02 13:49:45 -0700

    Update tf_object_detector.py

--- research/object_detection/tf_object_detector.py
+++ research/object_detection/tf_object_detector.py
@@ -39,7 +39,7 @@ from utils import visualization_utils as vis_util
 '''
 
 # What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
@@ -47,7 +47,8 @@ DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = 'INSERT PATH TO .PBTXT LABEL FILE CORRESPONDING TO LOADED MODEL'
+PATH_TO_LABELS = './labels/mscoco_label_map.pbtxt'                           # INSERT PATH TO .PBTXT LABEL 
+                                                                             # FILE CORRESPONDING TO LOADED MODEL
 
 # Change number of classes to match number in .pbtxt file
 NUM_CLASSES = 90
@@ -80,7 +81,8 @@ with detection_graph.as_default():
     dictionary mapping integers to appropriate string labels would be fine
 '''
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+# Path to labels can be changed to other label files that correspond to the loaded model
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)  
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
